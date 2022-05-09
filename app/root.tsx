d6b98c12c057f84587b9495d5404fcccd66df113
Daniel Reichhart <daniel.reichhart@gmx.net>
2022-05-09 12:07:18 +0200

    fix

--- app/root.tsx
+++ app/root.tsx
@@ -1,20 +1,26 @@
-import type { MetaFunction } from '@remix-run/node';
+import type { LinksFunction,MetaFunction } from '@remix-run/node';
 import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
+
+import tailwindStylesheetUrl from './styles/tailwind.css';
+
+export const links: LinksFunction = () => {
+    return [{ rel: 'stylesheet', href: tailwindStylesheetUrl }];
+};
 
 export const meta: MetaFunction = () => ({
     charset: 'utf-8',
-    title: 'New Remix App',
+    title: 'Cafés',
     viewport: 'width=device-width,initial-scale=1',
 });
 
 export default function App() {
     return (
-        <html lang="en">
+        <html lang="en" className="h-full">
             <head>
                 <Meta />
                 <Links />
             </head>
-            <body>
+            <body className="h-full">
                 <Outlet />
                 <ScrollRestoration />
                 <Scripts />
